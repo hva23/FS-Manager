2bfd460b63b08d499ca4eca9a7b3ce26af24bc22
hva23 <haskihossein@gmail.com>
2023-10-18 21:34:02 +0330

    "stringValueOf" function added

--- include/FileStatus.h
+++ include/FileStatus.h
@@ -1,3 +1,7 @@
+#ifndef Arduino_h
+#include <Arduino.h>
+#endif
+
 #ifndef FILE_STATUS_H
 #define FILE_STATUS_H
 #endif
@@ -13,4 +17,22 @@ public:
                 CanNotOpen,
                 Failed
         };
+        static String stringValueOf(FileStatus::Value status)
+        {
+                switch (status)
+                {
+                case FileStatus::Success:
+                        return "Success";
+                case FileStatus::DoesNotExist:
+                        return "DoesNotExist";
+                case FileStatus::AlreadyExists:
+                        return "AlreadyExists";
+                case FileStatus::CanNotOpen:
+                        return "CanNotOpen";
+                case FileStatus::Failed:
+                        return "Failed";
+                default:
+                        return "";
+                }
+        }
 };
